edd1923cadc0337966a7a341d6bf49d61e037352
TanSuChin <tansuchin23@gmail.com>
2026-04-15 03:19:05 +0800

    class

---
+++ class.dart
@@ -0,0 +1,78 @@
+class Person {
+  String name;
+  int age;
+
+  /*
+  constructor
+  constructor name = person, required parameter = name, age
+  age because got [] is optional parameter, and the default value if no value given is 18
+  
+  Person(String name, [int age=18]) {
+    this.name is the object variable above = name the input parameter
+    means store input values into object
+    this.name = name;
+    this.age = age;
+  }
+  constructor above get error but in Dart, proper way to write constructor is like this*/
+  Person(this.name, [this.age = 18]); 
+  
+  //named constructor shortcut
+  Person.guest() :
+    name = 'Guest',
+    age = 18;
+  
+
+  void showOutput() {
+    //print object data
+    print(name);
+    print(age);
+  }
+}
+
+class Vehicle {
+  String model;
+  int year;
+
+  Vehicle(this.model, this.year) {
+    print(this.model);
+    print(this.year);
+  }
+
+  void showOutput () {
+    print(model);
+    print(year);
+  }
+}
+
+//inheritance
+class Car extends Vehicle {
+  double price;
+
+  Car(String model, int year, this.price) : super(model, year);
+
+  void showOutput() {
+    super.showOutput();
+    print(this.price);
+  }
+}
+
+
+void main() {
+  /*create object
+  Person = type
+  person1 = variable name
+  Person('Kayla') = creating object
+  */
+  Person person1 = Person('Kayla', 10);
+  person1.age = 35;
+  person1.showOutput();
+
+  var person2 = Person('Jack', 76);
+  person2.showOutput();
+
+  var person3 = Person.guest();
+  person3.showOutput();
+
+  var car1 = Car('Accord', 2014, 150000);
+  car1.showOutput();
+}
--- function.dart
+++ function.dart
@@ -1,10 +1,23 @@
+/*
+diff between anonymous function and named function
+anonymous function : no name, used only once, shorter, more common in dart/flutter
+named function: has name, reusable, can be called anywhere
+
+positional parameter = used position to determine which value to pass into which parameter
+named parameter used {} specifically use the name to pass the value into the parameter
+*/ 
 void main() {
 showOutput(square(2.11));
 showOutput(square(2));
 
 var list = ['apples', 'bananaes', 'oranges'];
 
-list.forEach(printF);
+/*this is anonymous function
+a function without a name, created on the spot, can used only here
+*/
+list.forEach((item) {
+  print(item);
+});
 }
 
 //dynamic means this function can return any type of value
@@ -20,6 +33,10 @@ void showOutput(var msg) {
   print(msg);
 }
 
+/*this is named function
+printF is a named function and we pass it to forEach and 
+Dart calls printF for every item
 void printF(item) {
   print(item);
 }
+*/
