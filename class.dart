69fe07358c65173f8a069f2436a581a4fa0fd11d
TanSuChin <tansuchin23@gmail.com>
2026-04-15 03:37:23 +0800

    getter and setter

--- class.dart
+++ class.dart
@@ -56,6 +56,26 @@ class Car extends Vehicle {
   }
 }
 
+class X {
+  String name;
+  X(this.name);
+  void showOutput() {
+    print(this.name);
+  }
+  dynamic square(dynamic val) {
+    return val * val;
+  }
+}
+
+class Y extends X {
+  Y(String name) : super(name);
+
+  @override
+  void showOutput() {
+    print(this.name);
+    print('Hello');
+  }
+}
 
 void main() {
   /*create object
